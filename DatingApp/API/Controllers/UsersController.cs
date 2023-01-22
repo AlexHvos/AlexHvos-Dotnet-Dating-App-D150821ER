ec64758baa934064aca013bf647cb173c6a069a8
Alex Hvostikov <alexkhvostikov@gmail.com>
2023-01-22 22:01:24 +0200

    11. Adding a new api controller

--- DatingApp/API/Controllers/UsersController.cs
+++ DatingApp/API/Controllers/UsersController.cs
@@ -1,11 +1,23 @@
-Inside the controllers folder create new api controller named userscontroller
-Go to UsersController class and add the following:
-private readonly DataContext _context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _context;
         public UsersController(DataContext context) {
             _context = context;
 
-
         }
+        
         [HttpGet]
         public ActionResult<IEnumerable<AppUser>> GetUsers()
         {
@@ -13,14 +25,12 @@ private readonly DataContext _context;
             return users;
         }
 
-
         [HttpGet("{id}")]
-
 
         public ActionResult<AppUser> GetUser(int id)
         {
             var user = _context.Users.Find(id);
             return user;
         }
-
-Now when you use the api it can show the list of users or a specific user by adding api/users or api/users/’id number’ in the address of api page
+    }
+}
